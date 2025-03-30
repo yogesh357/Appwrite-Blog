73c90a0248f480d7bbdc7b92b90803829ad320cc
dhakaneyogesh <148894509+dhakaneyogesh@users.noreply.github.com>
2025-03-30 12:02:03 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container"
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo"
+import RTE from "./RTE";
+import Signup from "./Signup";
+
+import Login from "./Login"; 
+
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout"; 
+import Button from "./Button"
+import Input from "./Input";
+import Select from "./Select"; 
+
+export{
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    RTE,
+    Signup,
+    Login, 
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Button,
+    Input,
+    Select, 
+}
