2043453d7bb60ac8fa19b769bdfe55793013c887
dhakaneyogesh <148894509+dhakaneyogesh@users.noreply.github.com>
2025-04-09 21:00:32 +0530

    updated

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,55 +1,96 @@
-import React from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller } from 'react-hook-form';
+// import React from 'react'
+// import {Editor } from '@tinymce/tinymce-react';
+// import {Controller } from 'react-hook-form';
 
 
-export default function RTE({name, control, label, defaultValue =""}) {
+// export default function RTE({name, control, label, defaultValue =""}) {
+//   return (
+//     <div className='w-full'> 
+//     {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+//     <Controller
+//     name={name || "content"}
+//     control={control}
+//     render={({field: {onChange}}) => (
+//         <Editor
+//         initialValue={defaultValue}
+//         init={{
+//             initialValue: defaultValue,
+//             height: 500,
+//             menubar: true,
+//             plugins: [
+//                 "image",
+//                 "advlist",
+//                 "autolink",
+//                 "lists",
+//                 "link",
+//                 "image",
+//                 "charmap",
+//                 "preview",
+//                 "anchor",
+//                 "searchreplace",
+//                 "visualblocks",
+//                 "code",
+//                 "fullscreen",
+//                 "insertdatetime",
+//                 "media",
+//                 "table",
+//                 "code",
+//                 "help",
+//                 "wordcount",
+//                 "anchor",
+//             ],
+//             toolbar:
+//             "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+//             content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+//         }}
+//         onEditorChange={onChange}
+//         />
+//     )}
+//     />
+
+//      </div>
+//   )
+// }
+
+
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { Controller } from 'react-hook-form';
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
-    <div className='w-full'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+    <div className='w-full'>
+      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
 
-    <Controller
-    name={name || "content"}
-    control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
-    )}
-    />
-
-     </div>
-  )
+      <Controller
+        name={name || "content"}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field }) => (
+          <Editor
+            apiKey='uyndisdw4lkd8hypdhlbm3gawe1i04mag9mtg8q9yvtbwt0n'
+            value={field.value || defaultValue}
+            onEditorChange={(content) => {
+              field.onChange(content);
+            }}
+            init={{
+              height: 500,
+              menubar: true,
+              plugins: [
+                "advlist", "autolink", "lists", "link", "image", "charmap", "preview",
+                "anchor", "searchreplace", "visualblocks", "code", "fullscreen",
+                "insertdatetime", "media", "table", "help", "wordcount"
+              ],
+              toolbar:
+                "undo redo | blocks | " +
+                "bold italic forecolor | alignleft aligncenter alignright alignjustify | " +
+                "bullist numlist outdent indent | removeformat | help | image",
+              content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+            }}
+          />
+        )}
+      />
+    </div>
+  );
 }
-
